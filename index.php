45f0e25c54f5f52640414277f17ecd9913da4237
Vasudha Chalasani <40867028+vasuchal@users.noreply.github.com>
2020-08-16 23:05:28 -0500

    edits to php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
  	 <link rel="icon" href="logo.png" type="image/gif" sizes="16x16">
  	 <link href="index1.css" rel="stylesheet" type="text/css"/>
-	 <!-- <script src="website.js"></script>-->
+	 <script src="index.js"></script>
   	<style>
   	  div.topnav {
     	box-shadow: 0px 6px 6px grey;
@@ -51,7 +51,11 @@
 
 	</head>
 	<body>
-		<h1 style="color:#800020;font-size:40px;text-align:center;">About Recollect</h1>
+	<?php
+		echo"<h1 style='color:#800020;font-size:40px;text-align:center;'>About Recollect</h1>";
+		
+		?>
+		
 		<div id="indexHomepage"> 
 			<br>
 			<p> <strong>Recollect</strong> is an interactive tool for Alzheimers patients and their caregivers to facilitate their everyday activities.
@@ -65,16 +69,6 @@
 			<div id="disclaimer">
 			</div>
 		</div>
-	<script>
-
-		function Disclaimer(){
-				document.getElementById("disclaimer").innerHTML = "<p>&nbsp&nbspDisclaimer: Recollect is not a medical application or site, and will not help you improve or cure the chronic medical condition of Alzheimer's. Its purpose is to enhance the quality of life for those affected by Alzheimer's directly and indirectly. Recollect will not be responsible for any personal damage caused by the website or its dependents, and neither will we be liable for any damage to electronic equipment which is involved with our programs. By using this product, users understand and agree to the guidelines for use detailed above. The Recollect team maintains this website tofacilitate the lives of those afflicted with Alzheier's and their caretakers. This is a service that is continually under development. We will make every effort to keep this site current and to correct errors brought to our attention. The documents on this site contain links to information created and maintained by other public and private organizations. Please be aware that we do not control or guarantee the accuracy, relevance, timeliness, or completeness of this outside information. Further, the inclusion of links to particular items is not intended to reflect their importance, nor is it intended to endorse any views expressed or products or services offered by the author of the reference or the organization operating the site on which the reference is maintained. Translations of any materials into languages other than English are intended solely as a convenience to the non-English-reading public and are not legally binding. We have attempted to provide an accurate translation of the original material in English, but due to the nuances in translating to a foreign language, slight differences may exist. References identifying the original document in English are&nbsp&nbsp available in most non-English documents.&nbsp&nbsp</p><br>"
-				document.getElementById("disclaimer").style.fontSize = "10px";
-				document.getElementById("disclaimer").style.visibility = "visible";
-				
-
-		}
-
-	</script>
+	
 	</body>
 </html>
