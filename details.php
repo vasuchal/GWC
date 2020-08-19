5ccf47a245c1da53bca950000843b5b5760fe3cc
Vasudha Chalasani <40867028+vasuchal@users.noreply.github.com>
2020-08-18 20:00:10 -0500

    layout edits

--- details.php
+++ details.php
@@ -11,12 +11,20 @@ body{
     font-family: "Times New Roman", Times, serif;
 
 }
+
+
 input[type=text], input[type=password] {
-    width: 100%;
+    width: 70%;
     padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
+    margin: 30px 0;
     border: 1px solid #C70039;
+    display: inline-block;
+	text-align: left;
+	float: right;
+	border-radius:50px;
+	margin-top:-20px;
+	font-family: "Times New Roman", Times, serif;
+
 }
 
 /* Set a style for all buttons */
@@ -43,19 +51,19 @@ h1 {
 	font-size:40px;
 	color:#800020;
 	text-align:center;
-	animation: emphasize 2s 1;
+	animation: fade 4s 1;
 
 }
 
 
 
-@keyframes emphasize{
+@keyframes fade{
 
 	from {
-	    font-size:35px;
+	    opacity:0;
 	  }
 	to {
-    	font-size:40px;
+    	opacity:1;
 	  }	
 
 
@@ -78,47 +86,121 @@ fieldset {
   border-color: #800020;
   color:white;
   background-color: #800020;
+  width:70%;
+  margin:auto;
   }
 
-div{
-	margin-left:13%;
+#buttons{
+	margin-left:12%;
 }
 
+label{
+	
+	width:300px;
+	display:inline-block;
+	text-align:left;
+
+
+}
+
+#gear1{
+position:absolute;
+top:130px;
+left:60px;
+	
+}
+
+#gear2{
+position:absolute;
+top:50px;
+left:60px;
+}
+	
+#gear3{	
+position:absolute;
+top:90px;
+left:30px;
+}
+
+#gear4{
+position:absolute;
+top:450px;
+right:50px;
+
+}
+
+#gear5{
+position:absolute;
+top:370px;
+right:50px;
+
+}
+
+#gear6{
+position:absolute;
+top:410px;
+right:20px;
+
+}
+
+#gear1, #gear2, #gear3, #gear4, #gear5, #gear6{
+	animation: rotation 15s infinite linear;
+	animation-delay: 1s;
+
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
 </style>
 <body>
- 
-  <h1> CareTaker Account Information </h1>
+<img id="gear2" src ="gear2.png" height="80" width="80">
+<img id="gear1" src ="gear1.png" height="80" width="80">
+<img id="gear3" src ="gear3.png" height="80" width="80">
+<img id="gear5" src ="gear2.png" height="80" width="80">
+<img id="gear4" src ="gear1.png" height="80" width="80">
+<img id="gear6" src ="gear3.png" height="80" width="80">
+  <h1> CareTaker Account Information </h1>  
+<hr style="height:2px;color:#800020;background-color:#C70039;width:70%;">
+
   <!--I added a horizontal line because it divides up the different parts of the form and makes it look more clean and organized.-->
   <!--Below is my code for the text-fields, and they ask for Personal Information to place the pizza order. I also connected it to my other html webpage, so that when you click on submit it leads you to a thank you page.-->
 <form action="<?php echo $_SERVER["PHP_SELF"];?>" enctype="multipart/form-data" method="post">
    <fieldset>
     <br>
-    First Name: <input name="first" type="text" autofocus required><br>
-    Last Name: <input name="last" type="text" autofocus required><br>
-    Phone Number:<input name= "phone" type="text" autofocus required><br>
+    <br>
+    <label>First Name:</label> <input name="first" type="text" autofocus required><br><br>
+   <label> Last Name: </label><input name="last" type="text" autofocus required><br><br>
+   <label> Phone Number:</label><input name= "phone" type="text" autofocus required><br><br>
+    <br>
     <br>
     </fieldset>
     <!--I added another horizontal rule.-->
-  <hr style="height:2px;border-width:2;color:#800020;background-color:#C70039">
-
+    <br>
+  <hr style="height:2px;color:#800020;background-color:#C70039;width:70%;">
   <br>
   <fieldset>
-   Username: <input name="username" type="text" autofocus required><br>
-   Password: <input name="password" id="password" type="text" autofocus required><br>
-   Confirm Password: <input id="confirmation" type="text" autofocus required onchange="Confirm()">
+  <br>
+  <br>
+  <label> Username: </label> <input name="username" type="text" autofocus required><br>
+  <label> Password: </label><input name="password" id="password" type="text" autofocus required><br>
+  <label> Confirm Password:</label> <input id="confirmation" type="text" autofocus required onchange="Confirm()">
    <p id="confirmationMessage"></p>
    </fieldset>
 
   <br>
+
   <!--Below are my buttons for submit and reset which I learned from W3 schools.-->
-  <div>
+  <div id="buttons">
   <button onclick="location.href='signup.html'" type="button">Return to Previous Page</button> &nbsp &nbsp &nbsp
    <button type="submit" value="Submit">Submit</button> &nbsp &nbsp&nbsp&nbsp
     <button type="reset" value="Reset">Reset</button>
-    <div>
+    </div>
   </form>
-<script>
-
-
-
-</script>
+  
+</body>
--- patientsignin.php
+++ patientsignin.php
@@ -9,11 +9,15 @@ body{
 
 }
 input[type=text], input[type=password] {
-    width: 100%;
+    width: 70%;
     padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
+    margin: 30px 0;
     border: 1px solid #C70039;
+    display: inline-block;
+	text-align: left;
+	float: right;
+	border-radius:50px;
+	margin-top:-20px;
 }
 
 /* Set a style for all buttons */
@@ -39,19 +43,19 @@ h1 {
 	font-size:40px;
 	color:#800020;
 	text-align:center;
-	animation: emphasize 2s 1;
+	animation: fade 4s 1;
 
 }
 
 
 
-@keyframes emphasize{
+@keyframes fade{
 
 	from {
-	    font-size:30px;
+	    opacity:0;
 	  }
 	to {
-    	font-size:40px;
+    	opacity:1;
 	  }	
 
 
@@ -61,6 +65,8 @@ input[type=text] {
 	color:#800020;
 	border: 2px solid #800020; 
 	border-radius: 4px;
+	font-family: "Times New Roman", Times, serif;
+
 }
 
 input {
@@ -74,6 +80,8 @@ fieldset {
   border-color: #800020;
   color:white;
   background-color: #800020;
+  width:70%;
+  margin:auto;
   }
 
 div{
@@ -84,26 +92,80 @@ span{
 	font-size:9px;
 
 }
+
+label{
+	
+	width:300px;
+	display:inline-block;
+	text-align:left;
+	
+
+
+}
+
+#gear1{
+position:absolute;
+top:130px;
+left:60px;
+	
+}
+
+#gear2{
+position:absolute;
+top:50px;
+left:60px;
+}
+	
+#gear3{	
+position:absolute;
+top:90px;
+left:30px;
+}
+
+#gear1, #gear2, #gear3{
+	animation: rotation 15s infinite linear;
+	animation-delay: 1s;
+
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(359deg);
+  }
+}
+
+#buttons{
+	margin-left:12%;
+}
 </style>
 <body>
- 
+  <img id="gear2" src ="gear2.png" height="80" width="80">
+  <img id="gear1" src ="gear1.png" height="80" width="80">
+  <img id="gear3" src ="gear3.png" height="80" width="80">
   <h1> Patient Account Information </h1>
+  <hr style="height:2px;color:#800020;background-color:#C70039;width:70%;">
+  <br>
   <!--I added a horizontal line because it divides up the different parts of the form and makes it look more clean and organized.-->
   <!--Below is my code for the text-fields, and they ask for Personal Information to place the pizza order. I also connected it to my other html webpage, so that when you click on submit it leads you to a thank you page.-->
 <form action="<?php echo $_SERVER["PHP_SELF"];?>" enctype="multipart/form-data" method="post">
    <fieldset>
    <!--I added a legend tag which I learned from W3 schools, and I also learned how to use field-style.-->
-    First Name: <input type="text" autofocus required><br>
-    Last Name: <input type="text" autofocus required><br>
+    <br>
+    <label>First Name: </label><input type="text" autofocus required><br>
+    <label>Last Name: </label><input type="text" autofocus required><br>
+    <label>Password: </label><input value="Password will be the same as caretaker's" type="text" autofocus required><br>
+
     <br>
     </fieldset>
     <!--I added another horizontal rule.-->
-    <hr style="height:2px;border-width:2;color:#800020;background-color:#C70039">
 
 
   <br>
   <!--Below are my buttons for submit and reset which I learned from W3 schools.-->
-  <div>
+  <div id="buttons">
   <button onclick="location.href='details.php'" type="button">Return to Previous Page</button>&nbsp &nbsp&nbsp&nbsp
    <button type="submit" value="Submit">Submit<br> <span>(You will know be redirected to the Log In Page)</span></button>&nbsp &nbsp&nbsp&nbsp
     <button type="reset" value="Reset">Reset</button>
